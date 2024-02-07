9468826ee7dc69fb1cce69e979ab4c4c3d718cb0
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-02-07 17:25:40 +0200

    app
    basic functions

---
+++ src/components/NoContent.tsx
@@ -0,0 +1,40 @@
+const phrases = [
+  "No?",
+  "Are you sure?",
+  "Are you absolutely sure?",
+  "I'll be upset",
+  "Why are you doing this to me?",
+  "I'm gonna cry...",
+  "Who else if not you?",
+  "You'll break my heart",
+];
+
+type NoContentProps = {
+  noCount: number;
+};
+
+export const NoContent = ({ noCount }: NoContentProps) => {
+  const getNoButtonPhrase = () => {
+    return phrases[noCount % phrases.length];
+  };
+
+  return (
+    <div className="content">
+      {!noCount ? (
+        <video key={noCount} autoPlay loop muted width="200px" height="200px">
+          <source src={`../src/assets/video/main.mp4`} type="video/mp4" />
+        </video>
+      ) : (
+        <>
+          <video key={noCount} autoPlay loop muted width="200px" height="200px">
+            <source
+              src={`../src/assets/video/${(noCount % phrases.length) + 1}.mp4`}
+              type="video/mp4"
+            />
+          </video>
+          <p>{getNoButtonPhrase()}</p>
+        </>
+      )}
+    </div>
+  );
+};
